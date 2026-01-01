7c23e8f6afa4185fa02f3a547bc7edf8e3a31284
fikrinotes <fikrimulyanasetiawan@gmail.com>
2026-01-01 16:07:35 +0700

    add tab color in mobile version

--- src/app/quants-drill/layout.js
+++ src/app/quants-drill/layout.js
@@ -1,65 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+export const metadata = {
+  title: 'Quant Drill - Fikri Notes',
+  description: 'Train your quantitative thinking speed',
+  themeColor: '#050505',
+};
 
-
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang='id'>
-    {/* Global site tag (gtag.js) - Google Analytics */}
-    <head>
-    <link rel="icon" href="logo_fix.png" height="250px" />
-    <meta
-      name="google-site-verification"
-      content="JJt8s-bShfClSaIksS6BEHrJq7gFqDScaqKzsULaaH4"
-    />
-    <meta charSet="utf-8" />
-    <meta name="theme-color" content="#050505" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    {/* Primary Meta Tags */}
-    <title>Fikri Notes - Thought And Ideas Of Fikri Mulyana Setiawan</title>
-    <meta
-      name="title"
-      content="Fikri Notes - Though And Ideas Of Fikri Mulyana Setiawan"
-    />
-    <meta
-      name="description"
-      content="Fikri Mulyana Setiawan is a machine learning enthusiast, programmer and front-end developer."
-    />
-    {/* Open Graph / Facebook */}
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://fikrinotes.vercel.app/" />
-    <meta
-      property="og:title"
-      content="Fikri Notes - Thought And Ideas Of Fikri Mulyana Setiawan "
-    />
-    <meta
-      property="og:description"
-      content="Fikri Mulyana Setiawan is a programmer and front-end developer, and I like to automate everything with my programming skills."
-    />
-    <meta
-      property="og:image"
-      content="https://fikrinotes.netlify.app/metaTag.jpg"
-    />
-    {/* Twitter */}
-    <meta property="twitter:card" content="summary_large_image" />
-    <meta property="twitter:url" content="https://fikrinotes.vercel.app/" />
-    <meta
-      property="twitter:title"
-      content="Fikri Notes - Thought And Ideas Of Fikri Mulyana Setiawan"
-    />
-    <meta
-      property="twitter:description"
-      content="Fikri Mulyana Setiawan is a programmer and front-end developer, and I like to automate everything with my programming skills."
-    />
-    <meta
-      property="twitter:image"
-      content="https://fikrinotes.netlify.app/metaTag.jpg"
-    />
-    
-    </head>
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+export default function QuantsDrillLayout({ children }) {
+  return children;
 }
+
