6389f04379da063e69983647ef9f211bf70a1e60
fikrinotes <fikrimulyanasetiawan@gmail.com>
2026-01-01 16:25:05 +0700

    add tab color in mobile version

---
+++ src/app/layout.js
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: 'Quant Drill - Fikri Notes',
+  description: 'Train your quantitative thinking speed',
+  themeColor: '#050505',
+};
+
+export default function QuantsDrillLayout({ children }) {
+  return children;
+}
+
