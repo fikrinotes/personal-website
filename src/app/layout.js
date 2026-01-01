3daec1174814b0a6e6e355df1080e358c0324bdc
fikrinotes <fikrimulyanasetiawan@gmail.com>
2026-01-01 16:36:42 +0700

    add tab color in mobile version

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,38 @@
 export const metadata = {
-  title: 'Quant Drill - Fikri Notes',
-  description: 'Train your quantitative thinking speed',
-  themeColor: '#050505',
+  title: "Fikri Notes - Thought And Ideas Of Fikri Mulyana Setiawan",
+  description: "Fikri Mulyana Setiawan is a machine learning enthusiast, programmer and front-end developer.",
+  verification: {
+    google: "JJt8s-bShfClSaIksS6BEHrJq7gFqDScaqKzsULaaH4",
+  },
+  openGraph: {
+    type: "website",
+    url: "https://fikrinotes.vercel.app/",
+    title: "Fikri Notes - Thought And Ideas Of Fikri Mulyana Setiawan",
+    description: "Fikri Mulyana Setiawan is a programmer and front-end developer, and I like to automate everything with my programming skills.",
+    images: "https://fikrinotes.netlify.app/metaTag.jpg",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Fikri Notes - Thought And Ideas Of Fikri Mulyana Setiawan",
+    description: "Fikri Mulyana Setiawan is a programmer and front-end developer, and I like to automate everything with my programming skills.",
+    images: "https://fikrinotes.netlify.app/metaTag.jpg",
+  },
 };
 
-export default function QuantsDrillLayout({ children }) {
-  return children;
+export const viewport = {
+  themeColor: '#0A192F', // Warna global
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='id'>
+    <head>
+    <link rel="icon" href="logo_fix.png" height="250px" />
+    </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
 }
 
--- src/app/quants-drill/layout.js
+++ src/app/quants-drill/layout.js
@@ -1,7 +1,10 @@
 export const metadata = {
   title: 'Quant Drill - Fikri Notes',
-  description: 'Train your quantitative thinking speed',
-  themeColor: '#050505',
+  description: 'Train your quantitative thinking speeds',
+};
+
+export const viewport = {
+  themeColor: '#050505' // Warna global
 };
 
 export default function QuantsDrillLayout({ children }) {
